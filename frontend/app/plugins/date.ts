85c6a8acc12749e284fe321307c414f7e6096d47
Zadigo <pendenquejohn@gmail.com>
2025-07-21 13:46:23 +0200

    Add dayjs and error_handler

--- frontend/app/plugins/date.ts
+++ frontend/app/plugins/date.ts
@@ -1,9 +1,22 @@
 import dayjs from 'dayjs'
 
-export default defineNuxtPlugin(() => {
+import calendar from 'dayjs/plugin/calendar'
+import duration from 'dayjs/plugin/duration'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import timezone from 'dayjs/plugin/timezone'
+import utc from 'dayjs/plugin/utc'
+
+export default defineNuxtPlugin(nuxtApp => {
+  dayjs.extend(calendar)
+  dayjs.extend(duration)
+  dayjs.extend(utc)
+  dayjs.extend(timezone)
+  dayjs.extend(relativeTime)
+
   return {
     provide: {
-      dayjs
+      dayjs,
+      estimatedTimezone: dayjs.tz.guess()
     }
   }
 })
