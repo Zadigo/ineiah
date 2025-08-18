a74e7987e0d9b15d07254910d1f86d810746876c
Zadigo <pendenquejohn@gmail.com>
2025-08-18 12:47:10 +0200

    HTML sitemap

---
+++ frontend/app/pages/sitemap.vue
@@ -0,0 +1,23 @@
+<template>
+  <section id="sitemap" class="px-20">
+    <div class="grid grid-cols-2 my-20 py-20 gap-4">
+      <ShadCard v-for="section in footer.sections" :key="section.title" class="shadow-none bg-brand-pink-secondary-50">
+        <ShadCardContent>
+          <ul>
+            <h2 class="text-2xl mb-5 text-brand-secondary-700">{{ section.title }}</h2>
+            <li v-for="link in section.links" :key="link.name" class="py-1">
+              <NuxtLinkLocale :to="link.to" class="text-lg text-secondary underline">
+                {{ link.name }}
+              </NuxtLinkLocale>
+            </li>
+          </ul>
+        </ShadCardContent>
+      </ShadCard>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { footer } from '~/data'
+
+</script>
