cafd552c302074caea1c6caa49561fc31233e22e
Zadigo <pendenquejohn@gmail.com>
2026-03-14 12:50:48 +0100

    Send data to Google Analytics

--- frontend/app/plugins/errors.ts
+++ frontend/app/plugins/errors.ts
@@ -2,5 +2,16 @@ export default defineNuxtPlugin(nuxtApp => {
   nuxtApp.vueApp.config.errorHandler = (error, instance, info) => {
     // Handle errors to an external service here
     console.info('plugins/error_handler.ts', error, instance, info)
+
+    if (import.meta.client) {
+      // Log error to Google Analytics
+      const { sendEvent } = useAnalyticsEvent()
+      sendEvent(
+        defineAnalyticsEvent('exception', {
+          fatal: false,
+          description: `${error.message} at ${info}`
+        })
+      )
+    }
   }
 })
