031dee46ce06702ae602334c1d06366d98589dc8
Zadigo <pendenquejohn@gmail.com>
2026-05-03 22:12:57 +0200

    Code refactoring on plugins

--- frontend/app/plugins/errors.ts
+++ frontend/app/plugins/errors.ts
@@ -1,14 +1,13 @@
 export default defineNuxtPlugin(nuxtApp => {
-  nuxtApp.vueApp.config.errorHandler = (error, instance, info) => {
-    // Handle errors to an external service here
-    console.info('plugins/error_handler.ts', error, instance, info)
-
+  nuxtApp.vueApp.config.errorHandler = (error, _instance, info) => {
     if (import.meta.client) {
-      // Log error to Google Analytics
       const { sendEvent } = useAnalyticsEvent()
+
       sendEvent(
         defineAnalyticsEvent('exception', {
           fatal: false,
+
+          // @ts-ignore
           description: `${error.message} at ${info}`
         })
       )
