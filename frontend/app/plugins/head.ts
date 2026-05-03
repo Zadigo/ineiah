a228a52b65d9259d2d818186ed47e874989fec45
Zadigo <pendenquejohn@gmail.com>
2026-05-03 22:16:41 +0200

    Add doc on head

--- frontend/app/plugins/head.ts
+++ frontend/app/plugins/head.ts
@@ -5,7 +5,14 @@ export default defineNuxtPlugin(() => {
   const head = injectHead()
   const { get } = useBusinessDetails()
   
-  head.use(InferSeoMetaPlugin({
-    ogTitle: title => title?.replace(` - ${get('legalName')}`, '') || ''
-  }))
+  /**
+   * Generate Open Graph title by removing the legal name from the end of the page title. 
+   * This is because the legal name is already included in the og:site_name meta tag, and including 
+   * it in the og:title would be redundant.
+   */
+  head.use(
+    InferSeoMetaPlugin({
+      ogTitle: title => title?.replace(` - ${get('legalName')}`, '') || ''
+    })
+  )
 })
