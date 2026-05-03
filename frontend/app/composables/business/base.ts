48f269a4093d425cf5d3d3015ed4c846cf25618b
Zadigo <pendenquejohn@gmail.com>
2026-05-03 23:42:06 +0200

    Refactor business composable

--- frontend/app/composables/business/base.ts
+++ frontend/app/composables/business/base.ts
@@ -1,66 +1,5 @@
 import type { Nullable } from '~/types'
 
-export interface WebsiteProvider {
-  legalName: string
-  url: string
-}
-
-export interface CloudProvider extends WebsiteProvider {
-  description: string
-  address: string
-  rcs: string
-}
-
-export interface ContactPoints {
-  telephone: string
-  email: string
-  address: string
-}
-
-export type SocialPlatform = 'instagram' | 'facebook' | 'pinterest' | 'twitter' | 'linkedin' | 'tiktok' | 'youtube'
-
-export type Social = {
-  url: string
-  handle?: string
-}
-
-export interface BusinessDetails {
-  name: string
-  legalName: string
-  siren: string
-  siret: string
-  numberoTVA: Nullable<string>
-  creationDate: string
-  alternateName: string | string[]
-  description: string
-  logo: string
-  sameAs: string[]
-  image: string[]
-  rcs: string
-  address: {
-    street: string
-    postalCode: string
-    city: string
-    lat: number | null
-    lng: number | null
-  }
-  priceRange: '$' | '$$' | '$$$'
-  foundingDate: string
-  foundingLocation: string
-  founderImage: Nullable<string>
-  shareCapital: Nullable<string>
-  founder: string
-  founderDescription: string
-  founderKnowsAbout: string[]
-  webContentManager: string
-  publishingDirector: string
-  editorInChief: string
-  websiteProvider: WebsiteProvider
-  cloudProvider: CloudProvider
-  contact: ContactPoints
-  socials: Partial<Record<SocialPlatform, Social>>
-}
-
 export const businessDetails: BusinessDetails = {
   name: "La beauté d'Inéïah",
   legalName: "La beauté d'Inéïah",
@@ -148,12 +87,6 @@ export const businessDetails: BusinessDetails = {
   }
 }
 
-type BusinessDetailsKeys = keyof BusinessDetails
-
-type BusinessDetailsKeyValue = {
-  [K in BusinessDetailsKeys]: BusinessDetails[K]
-}
-
 /**
  * A composable to access business details throughout the application. It provides a `get` function
  * to retrieve specific details by key, ensuring type safety and consistency across the app.
@@ -175,7 +108,7 @@ export function useBusinessDetails() {
     const icons: Record<SocialPlatform, string> = {
       instagram: 'lucide:instagram',
       facebook: 'lucide:facebook',
-      pinterest: 'lucide:pinterest',
+      pinterest: 'fa7-brands:pinterest',
       twitter: 'lucide:twitter',
       linkedin: 'lucide:linkedin',
       tiktok: 'lucide:tiktok',
