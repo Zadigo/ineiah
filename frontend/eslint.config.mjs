b003b64166209c620fb950be05cfa7b87990513a
Zadigo <pendenquejohn@gmail.com>
2026-05-06 01:17:57 +0200

    Add eslint linkCheckerPlugin

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+// import linkCheckerPlugin from 'nuxt-link-checker/eslint'
 
 export default withNuxt({
   rules: {
     'vue/max-attributes-per-line': 'off'
   }
+  // plugins: [
+  //   linkCheckerPlugin
+  // ]
 })
